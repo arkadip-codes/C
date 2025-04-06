d29d96a9ccc0358da89607ea650f58ea45b98915
Arkadip Ghosh <arkadipghosh2005@gmail.com>
2025-04-06 22:50:20 +0530

    Update Prime.c

--- Prime.c
+++ Prime.c
@@ -3,8 +3,10 @@ int main() {
     int num, i = 2, isPrime = 1;
     printf("Enter a number: ");
     scanf("%d", &num);
-    while (i < num) {
-        if (num % i == 0) { 
+    while (i < num) 
+    {
+        if (num % i == 0) 
+        { 
             isPrime = 0;
             break;
         }
